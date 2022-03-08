eb845215a474a30afd1b1bfad824b1950862ee2c
Khushikam <94278433+Khushikam@users.noreply.github.com>
2022-03-08 00:03:49 -0800

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,26 @@
+import turtle
+color=['red','yellow','green','cyan','pink','white']
+t=turtle.Turtle()
+screen=turtle.Screen()
+screen.bgcolor('black')
+t.speed(25)
+for i in range(150):
+    t.color(color[i%6])
+    t.forward(i*1.5)
+    t.left(59)
+    t.width(3)
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+    
